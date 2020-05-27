8093d74aad5578ba63e1fd59642bdc459d1fe319
xiongshj <xiongshjcode@gmail.com>
2020-05-28 00:17:07 +0800

    feat: add TabBar and TabBarItem components

---
+++ src/components/tabbar/TabBar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="tab-bar">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TabBar'
+}
+</script>
+
+<style>
+#tab-bar {
+  position: fixed;
+  right: 0;
+  bottom: 0;
+  left: 0;
+
+  display: flex;
+  background: #f6f6f6;
+
+  box-shadow: 0 -1px 1px rgba(100, 100, 100, 0.2);
+}
+</style>
---
+++ src/components/tabbar/TabBarItem.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="tab-bar-item">
+    <slot name="item-icon"></slot>
+    <slot name="item-text"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TabBarItem'
+}
+</script>
+
+<style>
+.tab-bar-item {
+  flex: 1;
+  height: 49px;
+  font-size: 14px;
+  text-align: center;
+}
+
+.tab-bar-item img {
+  width: 24px;
+  height: 24px;
+  margin-top: 3px;
+  margin-bottom: 2px;
+  vertical-align: middle;
+}
+</style>
